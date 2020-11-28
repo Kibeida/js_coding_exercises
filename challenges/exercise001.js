90e40b6017fb335295785963a1bda20185274025
Kibeida <Kibeida@yahoo.com>
2020-11-28 16:06:46 +0000

    exercises one and two complete

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -1,7 +1,7 @@
 function capitalize(word) {
   if (word === undefined) throw new Error("word is required");
 
-  return word.substring(0, 1).toUpperCase() + word.substring(1 ,word.length);
+  return word.substring(0, 1).toUpperCase() + word.substring(1, word.length);
   // Add your code here!
 }
 
@@ -10,49 +10,130 @@ function generateInitials(firstName, lastName) {
   if (lastName === undefined) throw new Error("lastName is required");
   // Add your code here!
 
-  console.log(firstName.substring(0,1).toUpperCase() + "." + lastName.substring(0,1).toUpperCase());
-  return firstName.substring(0,1).toUpperCase() + "." + lastName.substring(0,1).toUpperCase();
+  return firstName.substring(0, 1).toUpperCase() + "." + lastName.substring(0, 1).toUpperCase();
 }
 
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
+  afterVat = (originalPrice / 100) * vatRate + originalPrice
+  if (afterVat % 1 === 0) {
+    return afterVat;
+  }
+  else {
+    return parseFloat(afterVat.toFixed(2));
+  }
+
+
+
   // Add your code here!
 }
 
 function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
+  reductionAmount = (originalPrice / 100) * reduction;
+  if (reductionAmount % 1 === 0) {
+    return originalPrice - reductionAmount;
+  } else {
+    parseFloat(reductionAmount.toFixed(2));
+    return originalPrice - parseFloat(reductionAmount.toFixed(2));
+
+  }
+
   // Add your code here!
+
 }
 
 function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
+
+  let position;
+  let length;
+
+  if (str.length % 2 == 1) {
+    position = str.length / 2;
+    length = 1;
+  }
+  else {
+    position = str.length / 2 - 1;
+    length = 2;
+  }
+
+
+  return str.substring(position, position + length);
+
   // Add your code here!
 }
 
 function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
+  let splitWord = word.split("");
+  let reverseWord = splitWord.reverse();
+  return reverseWord.join("");
+
   // Add your code here!
 }
 
 function reverseAllWords(words) {
   if (words === undefined) throw new Error("words is required");
+
+  const reverseWords = words.map(word => word.split("").reverse().join(""));
+
+  return reverseWords;
+
   // Add your code here!
 }
 
 function countLinuxUsers(users) {
   if (users === undefined) throw new Error("users is required");
+  linuxUsers = 0;
+
+  const filteredItems = users.filter((user) => {
+    //console.log(user.OS);
+    if (user.type === "Linux") {
+      linuxUsers++
+    }
+  })
+  return (linuxUsers);
+
+
   // Add your code here!
+
 }
 
 function getMeanScore(scores) {
   if (scores === undefined) throw new Error("scores is required");
   // Add your code here!
+  var total = 0;
+  for (let i = 0; i < scores.length; i++) {
+    total = total + scores[i];
+
+  }
+  if (total / scores.length % 1 === 0) {
+    return total / scores.length;
+  }
+  else {
+    let meanScore = (total / scores.length).toFixed(2);
+    return parseFloat(meanScore);
+  }
+
 }
 
 function simpleFizzBuzz(n) {
   if (n === undefined) throw new Error("n is required");
+  if (n % 5 === 0 && n % 3 === 0) {
+    return "fizzbuzz";
+  }
+  else if (n % 5 === 0) {
+    return "buzz";
+  }
+  else if (n % 3 === 0) {
+    return "fizz";
+  }
+  else {
+    return n;
+  }
   // Add your code here!
 }
 
--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,26 +1,54 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
   // Your code here!
+   
+  return sandwich.fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+  if (person.city == "Manchester") {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
   // Your code here!
+  return Math.ceil(people/40);
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
   // Your code here!
+  let sheep = 0;
+  arr.forEach(element => {
+    if (element == "sheep") {
+      sheep++;
+    }
+  });
+  return sheep;
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+  //let items = Object.values(person);
+//console.log(items);
+if (person.address.postCode.startsWith("M") && person.address.city == "Manchester" ){
+  //console.log(person.address.postCode);
+  return true;
+} else {
+  //console.log("false");
+  return false;
+}
+
+
+
 }
 
 module.exports = {
